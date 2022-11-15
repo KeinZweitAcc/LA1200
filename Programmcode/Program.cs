12cce7b501eee1cd8755b0684198029d83b42ae9
KeinZweitAcc <110885770+KeinZweitAcc@users.noreply.github.com>
2022-11-15 10:50:12 +0100

    Funktionen hinzugefügt

--- Programmcode/Program.cs
+++ Programmcode/Program.cs
@@ -14,6 +14,79 @@ namespace Lernatelier_Vokabeln
             int wordCount = 0;
             string filepath = @"words.txt";
 
+
+
+            testOrInput = AskUserForTestOrInput(isRepeat, testOrInput);
+
+            if (testOrInput == "a")
+            {
+                string file = File.ReadAllText(filepath);
+                string[] line = file.Split("\r\n");
+                wordCount = line.Length;
+                string[] wordsGerman = new string[wordCount];
+                string[] wordsForeignLanguage = new string[wordCount];
+
+                
+                for (int i = 0; i < line.Length - 1; i++)
+                {
+
+                    string[] word = line[i].Split(',');
+                    wordsGerman[i] = word[0];
+                    wordsForeignLanguage[i] = word[1];
+
+                }
+
+                maxScore = wordCount;
+                highscore = TestUserForWords(wordsGerman, wordsForeignLanguage, highscore, wordCount);
+
+                Console.WriteLine("Sie haben {0} von maximal {1} Punkte erreicht", highscore, maxScore);
+
+
+            }
+            else 
+            {
+
+                wordCount = AskUserForWordCount(wordCount, isRepeat);
+
+
+                string[] wordsGerman = new string[wordCount];
+                string[] wordsForeignLanguage = new string[wordCount];
+                isRepeat = true;
+
+
+                do
+                {
+                    try
+                    {
+
+                        for (int i = 0; i < wordCount; i++)
+                        {
+                            Console.WriteLine("Geben sie das deutsche Wort ein");
+                            wordsGerman[i] = (Console.ReadLine());
+                            Console.WriteLine("Geben sie die Übersetzung ein");
+                            wordsForeignLanguage[i] = (Console.ReadLine());
+                        }
+                        isRepeat = false;
+
+
+                    }
+                    catch
+                    {
+
+                        Console.WriteLine("Dies ist eine Ungültige Eingabe!");
+
+                    }
+
+                } while (isRepeat);
+
+
+                WriteWordsToAFile(wordsGerman, wordsForeignLanguage, wordCount, filepath);
+
+            }
+
+        }
+        static string AskUserForTestOrInput(bool isRepeat, string testOrInput)
+        {
             do
             {
                 try
@@ -41,138 +114,96 @@ namespace Lernatelier_Vokabeln
 
             } while (isRepeat);
 
-            if (testOrInput == "a")
-            {
-                string file = File.ReadAllText(filepath);
-                string[] line = file.Split("\r\n");
-                wordCount = line.Length;
-                string[] wordsGerman = new string[wordCount];
-                string[] wordsForeignLanguage = new string[wordCount];
+            return testOrInput;
+        }
+        static int AskUserForWordCount(int wordCount, bool isRepeat)
+        {
 
-                
-                for (int i = 0; i < line.Length - 1; i++)
+            do
+            {
+                try
                 {
 
-                    string[] word = line[i].Split(',');
-                    wordsGerman[i] = word[0];
-                    wordsForeignLanguage[i] = word[1];
+                    Console.Write("Wie viele wörter möchten sie eingeben? ");
+                    wordCount = Convert.ToInt32(Console.ReadLine());
+                    isRepeat = false;
+
+
+                }
+                catch
+                {
+
+                    Console.WriteLine("Dies ist eine Ungültige eingabe; Bitte geben sie nur ganze Zahlen ein!");
 
                 }
 
-                
+            } while (isRepeat);
 
-                for (int i = 0; i < line.Length - 1; i++)
+            return wordCount;
+        }
+        static int TestUserForWords(string[] wordsGerman, string[] wordsForeignLanguage, int highscore, int wordCount)
+        {
+
+            for (int i = 0; i < wordCount - 1; i++)
+            {
+                bool isAnswer = false;
+                do
                 {
-                    isAnswer = false;
-                    maxScore++;
-                    do
-                    {
 
-                        Console.WriteLine("Übersetzen sie bitte {0}", wordsGerman[i]);
-                        string input = Console.ReadLine();
-                        if (input == wordsForeignLanguage[i])
+                    Console.WriteLine("Übersetzen sie bitte {0}", wordsGerman[i]);
+                    string input = Console.ReadLine();
+                    if (input == wordsForeignLanguage[i])
+                    {
+                        Console.WriteLine("Dies ist Korrekt.");
+                        highscore++;
+                        isAnswer = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Dies ist leider nicht korrekt.");
+                        isAnswer = true;
+                        while (isAnswer == true)
                         {
-                            Console.WriteLine("Dies ist Korrekt.");
-                            highscore++;
-                            isAnswer = false;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Dies ist leider nicht korrekt.");
-                            isAnswer = true;
-                            while (isAnswer == true)
+                            Console.Write("Probieren sie es nochmal: ");
+                            input = Console.ReadLine();
+                            if (input == wordsForeignLanguage[i])
                             {
-                                Console.Write("Probieren sie es nochmal: ");
-                                input = Console.ReadLine();
-                                if (input == wordsForeignLanguage[i])
-                                {
 
-                                    Console.WriteLine("Dies ist Korrekt.");
-                                    isAnswer = false;
-                                }
-                                else
-                                {
-                                    Console.WriteLine("Dies ist leider inkorrekt, probieren sie es nochmal.");
-                                    isAnswer = true;
-                                }
+                                Console.WriteLine("Dies ist Korrekt.");
+                                isAnswer = false;
                             }
-
+                            else
+                            {
+                                Console.WriteLine("Dies ist leider inkorrekt, probieren sie es nochmal.");
+                                isAnswer = true;
+                            }
                         }
 
-                    } while (isAnswer == true);
-                }
-                Console.WriteLine("Sie haben {0} von maximal {1} Punkte erreicht", highscore, maxScore);
+                    }
 
-
+                } while (isAnswer == true);
             }
-            else 
-            {
-                
-                do
-                {
-                    try
-                    {
 
-                        Console.Write("Wie viele wörter möchten sie eingeben? ");
-                        wordCount = Convert.ToInt32(Console.ReadLine());
-                        isRepeat = false;
-
-
-                    }
-                    catch
-                    {
-
-                        Console.WriteLine("Dies ist eine Ungültige eingabe; Bitte geben sie nur ganze Zahlen ein!");
-
-                    }
-
-                } while (isRepeat);
-
-                
-                string[] wordsGerman = new string[wordCount];
-                string[] wordsForeignLanguage = new string[wordCount];
-                isRepeat = true;
-
-                
-                do
-                {
-                    try
-                    {
-
-                        for (int i = 0; i < wordCount; i++)
-                        {
-                            Console.WriteLine("Geben sie das deutsche Wort ein");
-                            wordsGerman[i] = (Console.ReadLine());
-                            Console.WriteLine("Geben sie die Übersetzung ein");
-                            wordsForeignLanguage[i] = (Console.ReadLine());
-                        }
-                        isRepeat = false;
-
-
-                    }
-                    catch
-                    {
-
-                        Console.WriteLine("Dies ist eine Ungültige Eingabe!");
-
-                    }
-
-                } while (isRepeat);
-
-                
-                string outputText = "";
-
-                
-                for (int i = 0; i < wordCount; i++)
-                {
-
-                    outputText += $"{wordsGerman[i]},{wordsForeignLanguage[i]}\r\n";
-
-                }
-                
-                File.WriteAllText(filepath, outputText);
-            }
+            return highscore;
 
         }
+        static void WriteWordsToAFile(string[] wordsGerman, string[] wordsForeignLanguage, int wordCount, string filepath)
+        {
+
+            string outputText = "";
+
+
+            for (int i = 0; i < wordCount; i++)
+            {
+
+                outputText += $"{wordsGerman[i]},{wordsForeignLanguage[i]}\r\n";
+
+            }
+
+            File.WriteAllText(filepath, outputText);
+
+        }
+
+
     }
 }
